a220a761a5754283001ba4b9be6f134a3bdffccd
BwinaRose <sabrinakrueger24@gmail.com>
2022-07-26 17:10:48 -0400

    created maven server

---
+++ java_social_server/src/main/java/bwina/java_social_server/JavaSocialServerApplication.java
@@ -0,0 +1,13 @@
+package bwina.java_social_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSocialServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSocialServerApplication.class, args);
+	}
+
+}
---
+++ java_social_server/src/test/java/bwina/java_social_server/JavaSocialServerApplicationTests.java
@@ -0,0 +1,13 @@
+package bwina.java_social_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSocialServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
