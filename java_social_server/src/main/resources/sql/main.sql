42d62f88ea618a563a9901de0e2d56345ac1f967
BwinaRose <sabrinakrueger24@gmail.com>
2022-07-27 00:51:17 -0400

    updated sql tables

--- java_social_server/src/main/resources/sql/main.sql
+++ java_social_server/src/main/resources/sql/main.sql
@@ -2,70 +2,86 @@ CREATE DATABASE `java_social`;
 
 use `java_social`;
 
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` varchar(50) primary key,
+    `email` varchar(50)
+);
+
+CREATE TABLE IF NOT EXISTS `profile`(
+    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+    `first_name` VARCHAR(50),
+    `last_name` VARCHAR(50),
+    `email` VARCHAR(50),
+    `user_name` VARCHAR(20),
+    `password` VARCHAR(16),
+    `sender_id` INTEGER,
+    `receiver_id` INTEGER,
+    `detail_id` INTEGER,
+    `group_id` INTEGER,
+    `admin_id` INTEGER
+);
 
 
-############################################################
+CREATE TABLE IF NOT EXISTS `user_details`(
+    `id` INTEGER PRIMARY KEY AUTO_INCREMENT
+);
 
-#
-# CREATE TABLE IF NOT EXISTS `message` (
-#     `id` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-#     `receiving_user_id` int,
-#     `sending_user_id` int,
-#     `content` varchar(256),
-#     `date` DATETIME
-#     );
-#
-# CREATE TABLE IF NOT EXISTS `posts` (
-#     `id` Integer primary key auto_increment,
-#     `content` varchar(200),
-#     `publishDate` DATE,
-#     `likes` Integer,
-#     `user_profile_id` Integer
-#     );
-#
-# CREATE TABLE IF NOT EXISTS `comments` (
-#     `id` Integer primary key auto_increment,
-#     `content` varchar(256),
-#     `user_profile_id` Integer,
-#     `post_id` Integer
-#     );
-#
-# CREATE TABLE IF NOT EXISTS `userprofiles` (
-#     `id` Integer primary key auto_increment,
-#     `first_name` varchar(50),
-#     `last_name` varchar(50),
-#     `email` varchar(50),
-#     `skills` varchar(50),
-#     `accomplishments` varchar(50),
-#     `group_id` Integer
-#     );
-#
-# CREATE TABLE IF NOT EXISTS `groups` (
-#     `id` Integer primary key auto_increment,
-#     `content` varchar(256),
-#     `owner_id` bigint not null,
-#     `post_id` Integer
-#     );
-#
-# CREATE TABLE IF NOT EXISTS `education` (
-#     `id` Integer primary key auto_increment,
-#     `school_name` varchar(50),
-#     `location` varchar(50),
-#     `start_date` varchar(10),
-#     `end_date` varchar(10),
-#     `field_of_study` varchar(50),
-#     `is_graduated` BOOLEAN,
-#     `grade_point_avg` DOUBLE,
-#     `user_profile_id` Integer
-#     );
-#
-# CREATE TABLE IF NOT EXISTS `experience` (
-#     `id` Integer primary key auto_increment,
-#     `job_title` varchar(50),
-#     `company` varchar(50),
-#     `location` varchar(10),
-#     `start_date` varchar(10),
-#     `end_date` varchar(50),
-#     `employment_type` varchar(50),
-#     `user_profile_id` Integer
-#     );
+CREATE TABLE IF NOT EXISTS `skills`(
+    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+    `skill` VARCHAR(50),
+    `detail_id` INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS `experience`(
+    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+    `employment_type` ENUM('FULL_TIME', 'PART_TIME', 'INTERNSHIP', 'APPRENTICESHIP', 'FREELANCE'),
+    `jobTitle` varchar(50),
+    `company` varchar(50),
+    `location` varchar(50),
+    `startDate` varchar(50),
+    `endDate` varchar(50),
+    `detail_id` INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS `education`(
+    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+    `schoolName` varchar(50),
+    `location` varchar(50),
+    `startDate` varchar(50),
+    `endDate` varchar(50),
+    `fieldOfStudy` varchar(50),
+    `isGraduated` Boolean,
+    `gradePointAvg` Double,
+    `detail_id` INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS `accomplishments`(
+    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+    `accomplishment` VARCHAR(50),
+    `detail_id` INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS `groups`(
+    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+    `groupName` varchar(20)
+);
+
+CREATE TABLE IF NOT EXISTS `posts`(
+    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+    `post_content` varchar(256),
+    `likes` INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS `messages`(
+    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+    `message_content` VARCHAR(256),
+    `date_sent` TIMESTAMP,
+    `post_id` INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS `comments` (
+    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+    `content` VARCHAR(500),
+    `date_posted` TIMESTAMP,
+    `post_id` INTEGER
+);
--- java_social_server/src/main/resources/sql/users.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS `users` (
-    `id` varchar(50) primary key,
-    `email` varchar(50)
-);
